37c538d659bf19aeb7e413ae21700b4d7622a304
13240137000 <13240137000@163.com>
2020-03-29 00:20:11 +0800

    created thumbnail module.

--- core/image/helper.py
+++ core/image/helper.py
@@ -1,7 +1,18 @@
+from conf.admin import ConfigManager
+from PIL import Image
+
+
 class ImageHelper(object):
 
-    def __get_images(self):
+
+    __config = ConfigManager()
+    __origin_path = __config.get_path_value("picture")
+    __target_path = __config.get_path_value("thumbnail")
+    __width = __config.get_picture_value("weight")
+    __height = __config.get_picture_value("height")
+
+    def __get_image_extension_name(self):
         pass
 
-    def resize(self):
+    def resize(self, name):
         pass
